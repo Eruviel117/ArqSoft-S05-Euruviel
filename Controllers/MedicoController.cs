4528f89677af72d6481fb565cd3ef4637b483184
Euruviel <euruviel.marquez@tecdesoftware.edu.mx>
2026-06-03 18:24:14 -0600

    feat: cascarón MVC con navegación manual vía URL

--- Controllers/MedicoController.cs
+++ Controllers/MedicoController.cs
@@ -1,6 +1,19 @@
-﻿namespace Citas_App.Controllers
+﻿using Citas_App.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Citas_App.Controllers
 {
-    public class MedicoController
+    public class MedicoController : Controller
     {
+        private readonly IMedicoRepository _repo;
+        public MedicoController(IMedicoRepository repo) { _repo = repo; }
+
+        public IActionResult Index() => View(_repo.ObtenerTodos());
+
+        public IActionResult Detalle(int id)
+        {
+            var medico = _repo.ObtenerPorId(id);
+            return medico == null ? NotFound() : View(medico);
+        }
     }
 }
